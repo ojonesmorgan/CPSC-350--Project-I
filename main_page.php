e03e793a79c18a9798a5cea9ac17602df83de729
unknown <ptaitt@mail.umw.edu>
2010-02-18 14:15:25 -0500

    added Owen's Main Page.

---
+++ main_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+  "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <title>Group: Decide on site title.</title>
+  <link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+
+<body>
+<div id="wrap">
+    <?php include("header.html"); ?>
+	<div id="main">
+	
+	<p><a href="add_band.php">Add Band</a></p>
+
+    
+    <p><a href="add_venue.php">Add Venue</a></p>
+
+  </form>
+	
+	
+	
+	
+	</div>
+	
+	
+	
+	
+    <?php include("sidebar.php"); ?>
+	<div id="footer"><p>"Footer" </p></div>
+</div>
+</body>
+</html>
