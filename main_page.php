0d54872ed57a0de1af53e1c8150af576ff18f06b
unknown <ptaitt@mail.umw.edu>
2010-02-23 13:39:04 -0500

    added scrum2.txt

--- main_page.php
+++ main_page.php
@@ -17,18 +17,56 @@
     
     <p><a href="addVenue.html">Add Venue</a></p>
 
-  </form>
+  
+	<?php
+	  include "db_connect.php";
+	
+	echo "<p class =\"side\"><b>Featured Band:</B><br/>";
+
+	  $query = "select bandName , bandState, bandCity, bandGenre, bandPhoto, bandDescription from band ORDER BY RAND() LIMIT 1";
+  
+	 $results = mysqli_query($db, $query);
+	while($row = mysqli_fetch_array($results)) {
+  	$bandName = $row['bandName'];
+  	$bandState = $row['bandState'];
+  	$bandCity = $row['bandCity'];
+	$bandGenre = $row['bandGenre'];
+	$bandPhoto= $row['bandPhoto'];
+	if (file_exists( $bandPhoto)== false){
+		//set $bandPhoto to default image
+		$bandPhoto="skull.jpg";
+		$altPhoto="Band Image Could Not Be Found";
+	}
+	else{
+		$altPhoto="$bandName's Photo";
+	}
+	$desc=$row['bandDescription'];
+	
+  	echo "
+	<table border=1 bordercolor=black>
+	<tr>
+		<td ><h3>$bandName<h3> ($bandGenre) </td>
+		<td ><table><tr><td><h4>From</h4></td></tr><tr><td>$bandCity , $bandState</td></tr></table> </td>
+	</tr>
+	<tr>
+		<td><img src=\"$bandPhoto\" alt=\"$altPhoto\" width=150 height=100></td>
+		<!--<td>Photo Path: bandPhoto </td>-->
+		<!-- Comments
+			makes a sizable image in the table with the path 'skull.jpg'
+			<td><img src=\"skull.jpg\" width = 150 height = 100></td>
+		-->
+		<td>$desc</td>
+	</tr>
+	</table>\n";
 	
 	
-	
-	
-	</div>
-	
-	
-	
+	}
+	?>	
+	</div>	
 	
     <?php include("projectSideBar.php"); ?>
 	<div id="footer"><p>"Footer" </p></div>
+</div>
 </div>
 </body>
 </html>
