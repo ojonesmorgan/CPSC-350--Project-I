d0356e4ff8ef9e76ca6d9a0e1efead1bd49d92e0
Matt <mmartin3@mail.umw.edu>
2010-03-14 23:19:38 -0400

    More enhanced sidebar fixes.

--- projectSideBar.php
+++ projectSideBar.php
@@ -2,14 +2,15 @@
 function search_panel($title, $id, $action, $msg)
 {
 	$on_focus = "onFocus=\"if (this.value == '$msg') { this.value = ''; } this.style.color = 'black';\"";
-	$on_blur = "onBlur=\"if(this.value == '') { this.value = '$msg'; this.style.color = 'gray'; }\"";
-	$searchbox .= "<input style=\"width:225px; display:block; margin-left:auto; margin-right:auto; color:gray; font-size:0.65em;\"";
+	$on_blur = "onBlur=\"if (this.value == '') { this.value = '$msg'; this.style.color = 'gray'; }\"";
+	$searchbox = "<input style=\"width:225px; display:block; margin-left:auto; margin-right:auto; color:gray; font-size:0.65em;\"";
 	$searchbox .= " $on_focus $on_blur id=\"$id\" name=\"q\" type=\"text\" value=\"$msg\" />";
 	$button = "<input style=\"width:225px; display:block; margin:10px auto 2px auto;\" type=\"submit\" value=\" Search \" />";
 	$search_panel = "<br />\n";
 	$search_panel .= "<fieldset style=\"border:2px solid red;\">\n";
 	$search_panel .= "<legend style=\"color:red; font-weight:bold;\">$title</legend>\n";
-	$search_panel .= "<form method=\"get\" action=\"$action\">\n";
+	$search_panel .= "<form onSubmit=\"if (this.q.value == '$msg') this.q.value = ''\" ";
+	$search_panel .= "method=\"get\" action=\"$action\">\n";
 	$search_panel .= "$searchbox\n$button\n";
 	$search_panel .= "</form>\n";
 	$search_panel .= "</fieldset>\n";
