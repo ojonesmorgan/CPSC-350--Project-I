4b1bd626812671dfbcbcbcd14c73c0f6ab6babcb
Amy Olson <amyolson43@gmail.com>
2010-03-21 19:47:08 -0400

    Database in 3NF, we are going to have to change almost every file to match the new database

--- ratings.sql
+++ ratings.sql
@@ -2,10 +2,10 @@ DROP TABLE IF EXISTS `ratings`;
 CREATE TABLE `ratings`
 (
   `rating` int(11) NOT NULL DEFAULT '0',
-  `bandName` varchar(60) NOT NULL,
-  `venueName` varchar(60) NOT NULL,
+  `band_id` varchar(60) NOT NULL,
+  `venue_id` varchar(60) NOT NULL,
   `email` varchar(50) NOT NULL,
-  FOREIGN KEY (`bandName`) REFERENCES band (`bandName`),
-  FOREIGN KEY (`venueName`) REFERENCES venue (`venueName`),
+  FOREIGN KEY (`band_id`) REFERENCES band (`band_id`),
+  FOREIGN KEY (`venue_id`) REFERENCES venue (`venue_id`),
   FOREIGN KEY (`email`) REFERENCES users (`email`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1 ;
