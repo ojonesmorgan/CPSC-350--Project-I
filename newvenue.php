ffb102be5d13b03fc4e1915972dea67e58e99797
Ptaitt <ptaitt@mail.umw.edu>
2010-03-24 08:08:56 -0700

    Add Venue Working with new database

--- newvenue.php
+++ newvenue.php
@@ -6,14 +6,68 @@ include("db_connect.php");
   
 $name = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['name']))));
 $street = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['street']))));
+$streetnum = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['streetnum']))));
 $city = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['city'])))); 
 $state = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['state']))));
+$zipcode = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['zipcode']))));
 $picture = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['picture']))));
 $description = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['description']))));
 $map = mysql_escape_string(stripslashes(htmlspecialchars(trim($_POST['map']))));
-  
-if (!empty($name)) mysqli_query($db, "INSERT INTO venue (venueName, venueState, venueCity, venueStreet, venueDescription, venuePicture, venueMap) VALUES ('$name', '$state', '$city', '$street', '$description', '$picture', '$map')");
-else header("location:addvenue.php?err=noname&name=$name&street=$street&city=$city&state=$state&description=$description&picture=$picture&map=$map");
+ 
+ //***********************************
+ //	<Check to see if zipcode already exists in DB>
+	if(!empty($zipcode)){
+		$quickQ="select zip_code from venue_zip_code where zip_code=$zipcode";
+		$quickR=mysqli_query($db,$quickQ);
+		$counter=0;
+		while ($quickRow= mysqli_fetch_array($quickR)){
+			$counter= $counter + 1;
+		}
+		if (counter == 0){ //zipcode is not already in the DB
+			mysqli_query($db, "INSERT INTO venue_zip_code (zip_code, city, state) 
+								VALUES ('$zipcode', '$city','$state')");		
+		}
+	}
+ //	</Check to see if zipcode already exists in DB>
+ //***********************************
+ //	<Insert Address Into DB>
+	if(!empty($street) or !empty($streetnum)){
+		mysqli_query($db,"INSERT INTO venue_address(number,street)
+							VALUES ('$streetnum','$street')");	
+	}
+ 
+ //	</Insert Address Into DB>
+ //***********************************
+ //	<Find Last Address Entered>
+		$quickQ="select address_id from venue_address order by address_id desc limit 1";
+		$quickR=mysqli_query($db,$quickQ);
+		while ($quickRow= mysqli_fetch_array($quickR)){
+			$addressID=$quickRow['address_id'];
+		}
+ //	</Find Last Address Entered>
+ //***********************************
+ //	<Insert New Band Into Database>
+/**if (!empty($name)){
+	mysqli_query($db, "INSERT INTO venue (venueName, venueState, venueCity, venueStreet, venueDescription, venuePicture, venueMap) 
+		VALUES ('$name', '$state', '$city', '$street', '$description', '$picture', '$map')");
+ }
+else header("location:addvenue.php?err=noname&name=$name&street=$street&city=$city&state=$state&description=$description&picture=$picture&map=$map"); 
+**/
+if (!empty($name)){
+	mysqli_query($db, "INSERT INTO venue (venueName, venueDescription, venuePicture, venueMap, venueZipCode,venueAddress_id) 
+		VALUES ('$name','$description', '$picture', '$map', '$zipcode','$addressID')");
+		
+		//<Find Laast Venue Entered>
+		$quickQ="select venue_id from venue order by venue_id desc limit 1";
+		$quickR=mysqli_query($db,$quickQ);
+		while ($quickRow= mysqli_fetch_array($quickR)){
+			$venueID=$quickRow['band_id'];
+		}
+		//</Find Laast Venue Entered>
+ }
+else header("location:addvenue.php?err=noname&name=$name&street=$street&city=$city&state=$state&description=$description&picture=$picture&map=$map"); 
+//	</Insert New Band Into Database> 
+//***********************************
 ?>
  
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
@@ -32,7 +86,7 @@ else header("location:addvenue.php?err=noname&name=$name&street=$street&city=$ci
 	
 <?php
 echo "<br /><h1>Thanks for adding the venue $name.</h1>";
-echo "<p><a href='venueprofile.php?name=$name'>Click here to go to the venue profile for $name.</a></p><br />\n";
+echo "<p><a href='venueprofile.php?id=$venueID'>Click here to go to the venue profile for $name.</a></p><br />\n";
 ?>
 	
 </div></center></div>
