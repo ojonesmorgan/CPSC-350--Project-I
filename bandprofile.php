6fe81e0b687baa3d01b274a6284a82bff62a1ed8
Ptaitt <ptaitt@mail.umw.edu>
2010-03-24 10:41:23 -0700

    bandprofile working

--- bandprofile.php
+++ bandprofile.php
@@ -2,29 +2,57 @@
 include("session.php");
 
 $saved = $_GET['saved'] == 1;
-$name = $_GET['name'];
+$bandID = $_GET['id'];
 
-if (empty($name))
+if (empty($bandID))
 {
 	header("location:musicsearch.php");
 	exit;
 }
 
-include("db_connect.php");
-$result = mysqli_query($db, "SELECT * FROM band WHERE bandName = '$name'");
-echo "band name: " .$name;
-$count = 0;
 
+include("db_connect.php");
+
+//**********************************
+$query="Select Genre from band b join genre g join band_genre bg where b.band_id=bg.band_id
+		and g.genre_id=bg.genre_id and b.band_id=" .$bandID;
+	//$query = "Select * from band b join genre g join band_genre bg where b.band_id=bg.band_id
+	//and g.genre_id=bg.genre_id ORDER BY RAND() LIMIT 1";
+	
+	$counter=0;
+	$results = mysqli_query($db, $query);	 
+	while($row = mysqli_fetch_array($results)) {
+	$counter=$counter+1;
+	
+		if ($counter==1){//1st genre
+			$genre1=$row['Genre'];	
+		}
+		if ($counter==2){//2nd genre
+			$genre2=$row['Genre'];
+		}
+		if ($counter==3){//3rd genre
+			$genre3=$row['Genre'];
+		}
+		if ($counter==4){//4th genre
+			$genre4=$row['Genre'];
+		}
+	}
+//**********************************
+$result = mysqli_query($db, "SELECT * FROM band WHERE band_id = '$bandID'");
+//echo "band name: " .$name;
+$count = 0;
 while ($row = mysqli_fetch_assoc($result))
 {
-	$genre = $row['bandGenre'];
-	$city = $row['bandCity'];
+	//$genre = $row['bandGenre'];
+	//$city = $row['bandCity'];
+	$name=$row['bandName'];
 	$state = $row['bandState'];
 	$description = $row['bandDescription'];
 	$photo = $row['bandPhoto'];
 
 	++$count;
 }
+echo "band name: " .$name;
 
 if ($count < 1)
 {
@@ -65,17 +93,39 @@ if ($count < 1)
 		echo "</p></fieldset>";
 	}
 	
-	if ($logged_in) echo "<form method='post' action='updateband.php?id=$name'>";
+	if ($logged_in) echo "<form method='post' action='updateband.php?id=$bandID'>";
 	echo "<p>";
 	//echo "<label for='name'>Band Name:</label> ";
 	//if ($logged_in) echo "<input name='name' type='text' value='$name' />";
 	//else echo "<a style='text-decoration:none;' name='name'>$name</a><br /><br />";
-	echo "<label for='genre'>Genre(s):</label> ";
-	if ($logged_in) echo "<input name='genre' type='text' value='$genre' />";
-	else echo "<a style='text-decoration:none;' name='genre'>$genre</a><br />";
-	echo "<br /><label for='city'>City:</label> ";
-	if ($logged_in) echo "<input name='city' type='text' value='$city' />";
-	else echo "<a style='text-decoration:none;' name='city'>$city</a><br />";
+	//*********************************************
+	//<Genre(s)>
+	echo "<br><label for='geners'>Genre(s)</label>";
+	//<1>
+	echo "<br><label for='genre1'>Genre 1:</label> ";
+	if ($logged_in) echo "<input name='genre1' type='text' value='$genre1' />";
+	else echo "<a style='text-decoration:none;' name='genre1'>$genre1</a><br />";
+	//</1>
+	//<2>
+	echo "<br><label for='genre2'>Genre 2:</label> ";
+	if ($logged_in) echo "<input name='genre2' type='text' value='$genre2' />";
+	else echo "<a style='text-decoration:none;' name='genre2'>$genre2</a><br />";
+	//</2>
+	//<3>
+	echo "<br><label for='genre3'>Genre 3:</label> ";
+	if ($logged_in) echo "<input name='genre3' type='text' value='$genre3' />";
+	else echo "<a style='text-decoration:none;' name='genre3'>$genre3</a><br />";
+	//</3>
+	//<4>
+	echo "<br><label for='genre4'>Genre 4:</label> ";
+	if ($logged_in) echo "<input name='genre4' type='text' value='$genre4' />";
+	else echo "<a style='text-decoration:none;' name='genre4'>$genre4</a><br />";
+	//</4>
+	//</Genre(s)>
+	//*********************************************
+	//echo "<br /><label for='city'>City:</label> ";
+	//if ($logged_in) echo "<input name='city' type='text' value='$city' />";
+	//else echo "<a style='text-decoration:none;' name='city'>$city</a><br />";
 	echo "<br /><label for='state'>State:</label> ";
 	if ($logged_in) echo "<input name='state' type='text' value='$state' />";
 	else echo "<a style='text-decoration:none;' name='state'>$state</a><br />";
