9219a70bba0212af095b4f1e0135b23bee6f6e8b
sjackson <Stephen@.(none)>
2010-04-08 12:21:05 -0400

    attempted fix database

--- databaseSource.sql
+++ databaseSource.sql
@@ -310,7 +310,34 @@ ALTER TABLE comments ADD FOREIGN KEY (track_id) REFERENCES tracks (track_id);
 ALTER TABLE ratings MODIFY band_id int(11) NOT NULL;
 ALTER TABLE ratings MODIFY venue_id int(11) NOT NULL;
 ALTER TABLE ratings ADD track_id int(11) NOT NULL AFTER venue_id;
-ALTER TABLE ratings ADD FOREIGN KEY (track_id) REFERENCES tracks (track_id);
+ALTER TABLE ratings ADD FOREIGN KEY (track_id) REFERENCES tracks 
+
+
+DROP TABLE IF EXISTS album_track;
+DROP TABLE IF EXISTS album;
+
+CREATE TABLE `album` (
+  `album_id` int(11) NOT NULL AUTO_INCREMENT,
+  `band_id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  PRIMARY KEY (`album_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+CREATE TABLE album_track
+(
+	album_track_id int(11) NOT NULL AUTO_INCREMENT,
+	album_id int(11) NOT NULL,
+	track_id int(11) NOT NULL,
+	track_number int(11) NOT NULL,
+	PRIMARY KEY (album_track_id),
+	FOREIGN KEY (album_id) REFERENCES album (album_id),
+	FOREIGN KEY (track_id) REFERENCES tracks (track_id)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+
+ALTER TABLE comments ADD COLUMN album_id int(11) NOT NULL;
+ALTER TABLE comments ADD FOREIGN KEY (album_id) REFERENCES album (album_id);
+ALTER TABLE ratings ADD COLUMN album_id int(11) NOT NULL AFTER track_id;
+ALTER TABLE ratings ADD FOREIGN KEY (album_id) REFERENCES album (album_id);
 
 --
 -- Dumping data for table `ratings`
